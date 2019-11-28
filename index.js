6954192869f20888f230af749cfccf88c594806b
Robertas <robertas.ankudovicius@ibm.com>
2019-11-28 00:54:46 +0000

    added user schema

--- index.js
+++ index.js
@@ -5,13 +5,12 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const initializeEnvVariables = require('./env');
 const Event = require('./models/event');
-
+const User = require('./models/user');
+const bcryptjs = require('bcryptjs');
 const app = express();
 initializeEnvVariables();
 
 app.use(bodyParser.json());
-
-const events = [];
 
 app.use('/graphql', graphqlHttp({
 	schema : buildSchema(`
@@ -24,6 +23,13 @@ app.use('/graphql', graphqlHttp({
 			date: String
 		}
 
+		type User {
+			_id:ID!
+			name: String!
+			email: String!
+			password: String!
+		}
+
 		input EventInput {
 			title:String!
 			description:String!
@@ -31,12 +37,20 @@ app.use('/graphql', graphqlHttp({
 			date: String
 		}
 
+		input UserInput {
+			name: String!
+			email: String!
+			password: String!
+		}
+
 		type RootQuery {
 			events: [Event]!
+			users: [User]!
 		}
 
 		type RootMutation {
 			createEvent(eventInput: EventInput): Event
+			createUser(userInput : UserInput): User
 		}
 
 		schema {
@@ -49,17 +63,53 @@ app.use('/graphql', graphqlHttp({
 			events : ()=>{
 				return Event.find();
 			},
+			users : ()=>{
+				return User.find();
+			},
 			createEvent : (args)=>{
+				let createdEvent;
+
 				const event = new Event({
 					title: args.eventInput.title,
 					description: args.eventInput.description,
 					price: args.eventInput.price,
-					date: new Date().toISOString()
+					date: new Date().toISOString(),
+					creator: '5ddf09479890456ef6173723'
 				});
+
 				return event.save().then(result =>{
-					return result
+					createdEvent = result;
+					return User.findById('5ddf09479890456ef6173723').then((user)=>{
+						if(!user){
+							throw new Error('User not found');
+						}
+						user.createdEvents.push(event);
+						return user.save();
+					}).then(()=>{
+						return createdEvent
+					});
+				})
+			},
+			createUser : (args)=>{
+
+				return User.findOne({email: args.userInput.email}).then((user)=>{
+					if(user){
+						throw new Error("This user already exist");
+					}
+					return bcryptjs.hash(args.userInput.password, 12);
+				}).then((hashedPassword)=>{
+					const user = new User({
+						name: args.userInput.name,
+						email: args.userInput.email,
+						password: hashedPassword
+					});
+					return user.save().then(result =>{
+						return result
+					}).catch(err=>{
+						console.log(err);
+					});
 				}).catch(err=>{
-					console.log(err);
+					throw err
 				});
 			}
 		},
@@ -81,5 +131,16 @@ app.use('/graphql', graphqlHttp({
 // 	createEvent(eventInput:{title: "Sky Park PArty", description: "Cool pae\rtyyy", price: 12.32}) {
 // 	  date
 // 	  title
+// 	}
+//   }
+
+
+
+
+
+// mutation{
+// 	createUser(userInput:{name: "Robertas", email: "r.ankudovicius@gmail.com", password: "myPassword"}){
+// 	  name
+// 	  email
 // 	}
 //   }
--- models/event.js
+++ models/event.js
@@ -6,6 +6,8 @@ const eventSchema =  new Schema({
     description:{type: String, required :true},
     price:{type: String, required :true},
     date:{type: String, required :true},
+    creator:{type:Schema.Types.ObjectId,
+        ref: 'User'}
 });
 
-module.exports = mongoose.model('Event', eventSchema);
+module.exports = mongoose.model('Event', eventSchema); 
